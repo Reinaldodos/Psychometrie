be20625fe25f30b672d2db72fbaa304119e0a016
REINALDO DOS SANTOS <rdossant@sfer.in.adc.education.fr>
2020-06-10 18:14:12 +0200

    reprise étude des DIF

--- FIMS DIF.R
+++ FIMS DIF.R
@@ -1,9 +1,7 @@
 pacman::p_load(TAM, psych, tidyverse, data.table, GLDEX)
 data("data.fims.Aus.Jpn.scored")
 input = 
-  data.fims.Aus.Jpn.scored%>% select(starts_with("M")) %>% 
-  select(-M1PTI12, -M1PTI14, -M1PTI21) 
-
+  data.fims.Aus.Jpn.scored %>% select(starts_with("M"))
 contexte = data.fims.Aus.Jpn.scored %>% 
   select(-starts_with("M")) %>% 
   mutate_all(.funs = as.factor)
@@ -12,7 +10,8 @@ DIF =
   tam.mml.mfr(
     resp = input,
     formulaA =  ~ item * (SEX + country),
-    facets = contexte
+    facets = contexte, 
+    verbose = F
   )
 
 DIFS = 
@@ -22,10 +21,31 @@ DIFS =
   mutate(p.value = pnorm(q = -abs(zscore))) %>%
   tidyr::extract(col = parameter,
                  into = c("item", "selon"),
-                 regex = "(.*):(.*)") %>%
-  filter(p.value < 0.05) %>% 
-  group_by(item, facet) %>% 
-  summarise(DIF = max(xsi)-min(xsi)) %>% ungroup 
+                 regex = "(.*):(.*)") 
 
-DIFS %>% 
-  spread(key = facet, value = DIF)
+DIFS %>%
+  ggplot(mapping = aes(x = item, colour = selon)) +
+  geom_linerange(
+    mapping = aes(ymin = xsi - 1.96 * se.xsi,
+                  ymax = xsi + 1.96 * se.xsi),
+    position = position_dodge(width = .2)
+  ) +
+  geom_hline(yintercept = 0) +
+  coord_flip() +
+  facet_wrap( ~ facet)
+
+
+DIFS %>%
+  group_by(item, facet) %>%
+  summarise(DIF = max(xsi) - min(xsi),
+            DIF_error = sqrt(sum(se.xsi^2))) %>% 
+  ggplot(mapping = aes(y = item, colour = facet)) +
+  geom_linerange(
+    mapping = aes(xmin = DIF - 1.96 * DIF_error,
+                  xmax = DIF + 1.96 * DIF_error),
+    position = position_dodge(width = .2)
+  ) +
+  geom_vline(xintercept = c(0,.5))
+
+
+
