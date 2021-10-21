29a57cd4e4a079dcbc76d6dedf9b9ccba0b161d9
REINALDO DOS SANTOS <rdossant@sfer.in.adc.education.fr>
2021-10-21 11:44:45 +0200

    Exemples pour Maroc

---
+++ prépa Maroc.R
@@ -0,0 +1,99 @@
+require(tidyverse)
+require(magrittr)
+
+get_pool <- function(seq) {
+  seq %>%
+    enframe(name = "item", value = "b") %>%
+    mutate(a = 1,
+           c = 0,
+           item = str_c("item", item)) %>%
+    return()
+}
+
+input = seq(-2, 2, 1) %>% get_pool()
+
+output = runif(n = 400, min = -2, max = 2) %>% get_pool()
+
+require(xxIRT)
+
+new_dnorm = function(...){
+  2*dnorm(...)}
+
+xxIRT::model_3pl_plot(a = input$a, b = input$b, c = 0)
+
+xxIRT::model_3pl_plot(a = input$a,
+                      b = input$b,
+                      c = 0,
+                      type = "info") +
+  geom_function(fun = new_dnorm,
+                args = list(mean = 0, sd = 1),
+                colour = "black")
+
+xxIRT::model_3pl_info(
+  a = input$a,
+  b = input$b,
+  c = 0, 
+  t = seq(-4,4,.5)) %>% view()
+
+
+# CAT ---------------------------------------------------------------------
+set.seed(1234)
+
+CAT =
+  rnorm(n = 1) %>%
+  xxIRT::cat_sim(
+    pool = output,
+    min = 1,
+    max = 50,
+    stop_se = .2
+  ) 
+
+CAT %>% plot() 
+
+CUT = qnorm(p = 1 / 3)
+CAT =
+  # rnorm(n = 1) %>%
+  -.6 %>%
+  cat_sim(
+    pool = output,
+    min = 1,
+    max = 100,
+    stop_cut = CUT
+  ) 
+
+CAT %>%
+  plot() +
+  geom_hline(yintercept = CUT, linetype = "dotted")
+
+
+# MST ---------------------------------------------------------------------
+
+MST = 
+  xxIRT::mst(
+    pool = output,
+    design = "1-2-3",
+    num_panel = 1,
+    method = "topdown", 
+    max_use = 1, len = 25
+  )
+
+
+MST = mst_obj(x = MST, theta = -2, indices = 1)
+MST = mst_obj(x = MST, theta = -1, indices = 2)
+MST = mst_obj(x = MST, theta = -.5, indices = 3)
+MST = mst_obj(x = MST, theta = .5, indices = 4)
+MST = mst_obj(x = MST, theta = 1, indices = 5)
+MST = mst_obj(x = MST, theta = 2, indices = 6)
+
+MST = mst_assemble(x = MST)
+MST %>% plot(byroute = T)
+MST %>% plot(byroute = F)
+
+MST$items %>% split(f = .$module)
+
+
+
+ATA = ata(pool = output, num_form = 13, max_use = 4, len = 20) 
+ATA = xxIRT::ata_obj_relative(x = ATA, coef = 0, mode = "max")
+
+ATA %>% ata_solve() %>% plot()
